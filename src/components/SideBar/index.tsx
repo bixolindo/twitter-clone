a9bc00eb992e2c94beda35fbac3512a11821b0fe
Gustavo Vasconcelos <gustavo.vas.bl@gmail.com>
2021-03-17 11:51:33 -0300

    :sparkles: SideBar ended

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import StickyBox from 'react-sticky-box'
+
+import List from '../List'
+import FollowSuggestion from '../FollowSuggestion'
+import News from '../News'
+
+import { Container, SearchWrapper, SearchInput, SearchIcon, Body} from './styles';
+
+const SideBar: React.FC = () => {
+  return (
+    <Container>
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+
+      <StickyBox>
+        <Body>
+        <List 
+          title="Talvez você curta"
+          elements={[
+            <FollowSuggestion name = "Nabil Duarte" nickname = "@fandodino" />,
+            <FollowSuggestion name = "Matheus Silva" nickname = "@matheuscomcincos" />,
+            <FollowSuggestion name = "Paulo Ozeia" nickname = "@pardaldejava" />
+          ]}
+        />
+        <List 
+          title="Talvez você curta"
+          elements={[<News />,<News />,<News />]}
+        />
+        <List 
+          title="Talvez você curta"
+          elements={[<News />,<News />,<News />]}
+        />
+        <List 
+          title="Talvez você curta"
+          elements={[<News />,<News />,<News />]}
+        />
+        </Body>
+      </StickyBox>
+    </Container>
+  );
+}
+
+export default SideBar;
